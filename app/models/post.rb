bd6aa853534ad4a2b65deaef5d43c1fa232ab619
denniscual <mamayandpapay@gmail.com>
2016-09-03 21:48:29 +0800

    removing should_generate_new_friendly_id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,5 @@
 class Post < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
-
-  def should_generate_new_friendly_id?
-    slug.blank? || self.title_changed?
-  end
-
-
+  
 end
